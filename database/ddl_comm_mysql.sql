8678cc3b3d439435ae704c09ff2f91f4f28a027f
market <*@g.nsu.ru>
2022-05-15 12:14:53 +0000

    DB_ddl_comm2

--- database/ddl_comm_mysql.sql
+++ database/ddl_comm_mysql.sql
@@ -3,3 +3,11 @@ CREATE TABLE users (
 	password varchar(100) NOT NULL,
 	PRIMARY KEY (username)
 );
+
+CREATE TABLE search_history (
+	content varchar(200) NOT NULL,
+	username varchar(100) NOT NULL,
+	content_date DATE NOT NULL
+);
+
+ALTER TABLE search_history ADD CONSTRAINT search_history_fk0 FOREIGN KEY (username) REFERENCES users(username);
