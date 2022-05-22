261d1ae8649105aa44a29cb99336bce6224a26be
root <root@market.ru-central1.internal>
2022-05-22 12:13:36 +0000

    Изменил стркутру проекта, изменил maven

--- database/sql_commands/dml_comm_test.sql
+++
@@ -1,5 +0,0 @@
-insert into users values("mmikhajlov", "123");
-insert into search_history values("Phones", "mmikhajlov", "2022-05-15");
-insert into search_history values("Phones", "mmikhajlov", "2021-05-15");
-insert into search_history values("Boxes", "mmikhajlov", "2021-05-15");
-
