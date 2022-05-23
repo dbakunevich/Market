1e5edb3068541771dcd89189d119a12c9163193c
unknown <m.mikhailov1@g.nsu.ru>
2022-05-23 17:53:26 +0700

    db_connectt

--- backend/src/main/java/spring/Main.java
+++ backend/src/main/java/spring/Main.java
@@ -1,5 +1,6 @@
 package spring;
 
+import database.DBWorker;
 import org.springframework.boot.*;
 import org.springframework.boot.autoconfigure.*;
 import org.springframework.ui.Model;
@@ -11,5 +12,6 @@ import org.springframework.web.bind.annotation.*;
 public class Main {
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
+        //DBWorker worker = new DBWorker();
     }
 }
