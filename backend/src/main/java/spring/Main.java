7027fc096f616d757516541fa51027fd475555db
root <root@market.ru-central1.internal>
2022-05-23 05:29:11 +0000

    Изменил структуру мавен проекта для БД

--- backend/src/main/java/spring/Main.java
+++ backend/src/main/java/spring/Main.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.*;
 
 @SpringBootApplication
 @RestController
+@EnableAutoConfiguration
 public class Main {
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
