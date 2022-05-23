1362015c374d722e1e36d9f1e2a28e2f01cc02a6
unknown <m.mikhailov1@g.nsu.ru>
2022-05-23 18:54:28 +0700

    сделал методы для бд

--- backend/src/main/java/spring/Main.java
+++ backend/src/main/java/spring/Main.java
@@ -6,12 +6,19 @@ import org.springframework.boot.autoconfigure.*;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
+
 @SpringBootApplication
 @RestController
 @EnableAutoConfiguration
 public class Main {
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
-        //DBWorker worker = new DBWorker();
+//        DBWorker worker = new DBWorker();
+//        ArrayList<String> arrayList = null;
+//        arrayList = worker.findHistory("mmikhajlov");
+//        for(String s : arrayList) {
+//            System.out.println(s);
+//        }
     }
 }
