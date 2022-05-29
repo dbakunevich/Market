fd044bb4a4406abed24466f6ecaa3c2d4cb6966e
Kireev-Vladimir <v.kireev@g.nsu.ru>
2022-05-29 20:12:57 +0700

    Saving parsing results

--- backend/src/main/java/spring/Main.java
+++ backend/src/main/java/spring/Main.java
@@ -5,12 +5,17 @@ import org.springframework.boot.*;
 import org.springframework.boot.autoconfigure.*;
 import org.springframework.web.bind.annotation.*;
 import parsing.BrowserPool;
+import parsing.Product;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @SpringBootApplication
 @RestController
 public class Main {
     
     public static DBWorker dbWorker = new DBWorker();
+    public static final Map<String, List<Product>> requestsMap = new HashMap<>();
 
     public static void main(String[] args) {
         String driverPath = "/usr/local/bin/geckodriver";
