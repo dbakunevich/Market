b97db25e0c71e8b652f1215e31d1c75b6cd7aab0
unknown <m.mikhailov1@g.nsu.ru>
2022-06-04 02:05:08 +0700

    добавление тестов для DBWorker

--- backend/src/test/java/database/TestDBWorker.java
+++ backend/src/test/java/database/TestDBWorker.java
@@ -10,13 +10,28 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.Random;
 import java.util.logging.Logger;
 
 
 public class TestDBWorker {
     static Connection connection_main;
 
-
+    public static String[] generateRandomWords(int numberOfWords)
+    {
+        String[] randomStrings = new String[numberOfWords];
+        Random random = new Random();
+        for(int i = 0; i < numberOfWords; i++)
+        {
+            char[] word = new char[random.nextInt(8)+3]; // words of length 3 through 10. (1 and 2 letter words are boring.)
+            for(int j = 0; j < word.length; j++)
+            {
+                word[j] = (char)('a' + random.nextInt(26));
+            }
+            randomStrings[i] = new String(word);
+        }
+        return randomStrings;
+    }
     @BeforeAll
     public static void init() {
         Logger logger = null;
@@ -69,9 +84,27 @@ public class TestDBWorker {
     @Test
     public void addUserTrueTest() {
         boolean bool;
+        String[] str;
         DBWorker worker = new DBWorker();
-        bool = worker.addUser("mmikhajlov2", "123");
+        str = generateRandomWords(4);
+        bool = worker.addUser(str[0] + str[1] + str[2] + str[3], "123");
         assertTrue(bool);
+    }
+
+    @Test
+    public void addUserFalseTest() {
+        boolean bool;
+        DBWorker worker = new DBWorker();
+        bool = worker.addUser("mmikhajlov", "123");
+        assertFalse(bool);
+    }
+
+    @Test
+    public void findUserTrueTest() {
+        String str;
+        DBWorker worker = new DBWorker();
+        str = worker.findUser("mmikhajlov", "123");
+        assertEquals(str, "mmikhajlov");
     }
 
     @AfterAll
