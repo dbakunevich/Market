bf9cf7898abf2d40e3ccf4e26704cc6808d965bb
Бакуневич Дмитрий Сергеевич <d.bakunevich@g.nsu.ru>
2022-06-03 21:44:15 +0000

    Update User.java

--- backend/src/main/java/database/User.java
+++ backend/src/main/java/database/User.java
@@ -4,9 +4,7 @@ public class User {
     private String username;
     private String password;
     private String role;
-
-    public User() {
-    }
+    
 
     public User(String username, String password) {
         this.username = username;
